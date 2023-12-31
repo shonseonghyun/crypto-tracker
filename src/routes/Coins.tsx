6676e0b18799df17984922fec1305b5a4201cc75
sunghyun7895 <sunghyun7895@naver.com>
2023-12-31 15:05:34 +0900

    CryptoTracker v1.1.1 - ReactQuery 사용

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,6 +1,8 @@
 import styled from "styled-components";
 import { Link } from "react-router-dom"; 
 import { useEffect, useState } from "react";
+import { useQuery } from "react-query";
+import { fetchCoins } from "../api/CoinsApi";
 
 
 const Container = styled.div`
@@ -21,11 +23,7 @@ const Loader = styled.span`
     display:block;
 `;
 
-
 const CoinList= styled.ul``;
-
-
-
 const Coin = styled.li`
     background-color:white;
     color:${props=>props.theme.bgColor};
@@ -52,7 +50,7 @@ const Img = styled.img`
     margin-right:10px;
 `;
 
-interface CoinInterface{
+interface ICoin{
     id:string,
     name:string,
     symbol:string,
@@ -63,24 +61,14 @@ interface CoinInterface{
 } 
 
 function Coins(){
-
-    const [coins,setCoins] = useState<CoinInterface[]>([]);
-    const [loading,setLoading] = useState(true);
-    useEffect(()=>{
-        (async()=>{
-            const response = await fetch("https://api.coinpaprika.com/v1/coins");
-            const json = await response.json();
-            setCoins(json.slice(0,700));
-            setLoading(false);
-        })();
-    },[])
+    const {isLoading,data} = useQuery<ICoin[]>("allCoins",fetchCoins);
 
     return (
         <Container>
             <Header>코인</Header>
-            {loading ? <Loader>Loading...</Loader>:
+            {isLoading ? <Loader>Loading...</Loader>:
             <CoinList>
-                {coins.map(coin=>
+                {data?.slice(0,100).map(coin=>
                     <Coin key={coin.id}>
                         <Link 
                         to={{
